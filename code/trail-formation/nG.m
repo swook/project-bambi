21e769b88bc833540a201b7a9997ea32cceb1be6
Seon-Wook Park <seon.wook@swook.net>
2013-11-08 12:12:49 +0100

    Change nG to use matrix of G

--- code/trail-formation/nG.m
+++ code/trail-formation/nG.m
@@ -1,13 +1,21 @@
-function [ nextG ] = nG( r,G,A_pos,Gzero,Gmax,I,T,dt )
-%dG time evolution of Ground 
+function [ nextG ] = nG(G, A_pos, Gzero, Gmax, I, T, dt)
+% dG time evolution of Ground
+%  G is nxn matrix of G values
 
-dG=Vregen( G,Gzero,T );
+% dG is nxn matrix
+dG = Vregen( G,Gzero,T );
 
-    for i=1:size(A_pos,1)
-        if r==A_pos
-           dG=dG+Itrample(G,Gmax,I);
-        end
-    end
-    
-    nextG=G+dt*dG;
+% Contribution from pedestrian trampling
+contribI = Itrample(G,Gmax,I);
+
+% Iterate over agents
+% - We can do this because agent only contributes to ground at its own position.
+for i = 1:size(A_pos, 1) % size(A_pos, 1) gives no. of rows of A_pos
+	x = A_pos(i, 1);
+	y = A_pos(i, 2);
+	dG(y, x) = dG(y, x) + contribI(y, x);
+end
+
+nextG = G + dt*dG;
+
 end
