51264397190040d9f5b77daccefa10f2ee0c302a
Seon-Wook Park <seon.wook@swook.net>
2013-11-08 12:13:17 +0100

    Simple initial visualisation

--- code/trail-formation/trail.m
+++ code/trail-formation/trail.m
@@ -2,21 +2,37 @@
 
 Gzero = 1;  % Initial G (comfort of walking) value
 Gmax = 10;  % Maximum comfort of walking
-I = 1;  % Intensity parameter of trampling
-T = 2;  % Durability of trails
+I = 5;  % Intensity parameter of trampling
+T = 10;  % Durability of trails
 sigma = 0.5;  %  Visibility function
 
 h = 100;  % Grid height
 w = 100;  % Grid width
-nagent = 7;  % Number of bambis
+nagent = 100;  % Number of bambis
 
-dt=1;   %time step
+dt = 0.1;   % Time step
 
 % Create environmental grid (matrix) and agent (bambis) information
+G = Gzero * ones(h,w);  % Environmental grid
 
-GridG = zeros(h,w);  % Environmental grid
-
+%% Initial agents placement
 dests = {[1 1], [100 100], [100 1], [1 100]};
-[A_pos, A_dest] = PlaceAgents(h, w, nagent, dests)  % Agents (bambis) positions matrix (x,y)
+[A_pos, A_dest] = PlaceAgents(h, w, nagent, dests);  % Agents (bambis) positions matrix (x,y)
 A_dir = zeros(nagent,2);  % Agents (bambis) direction (x,y)
 
+%% Run simulation
+for i = 1:1000
+	disp(i)
+
+	% Calculate next values of G
+	G = nG(G, A_pos, Gzero, Gmax, I, T, dt);
+
+	% Simple visualisation
+	surf(G);             % 3D contour plot of values
+	view(2);             % Set to bird's eye view
+	colormap(summer);    % Sets colour scale to go from green to yellow
+	colorbar;            % Shows the colour bar
+	caxis([Gzero Gmax]); % Sets axis limits of colour bar
+
+	pause(.1);
+end
