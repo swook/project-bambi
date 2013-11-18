fda2f3294765729587b21df4a792ff8713d34954
Seon-Wook Park <seon.wook@swook.net>
2013-11-18 18:00:40 +0100

    Make path-finding use newDest

--- code/path-finding/pathfind.m
+++ code/path-finding/pathfind.m
@@ -1,12 +1,23 @@
-function [G, A_pos] = pathfind(G, A_pos, v, dests)
+function [G, A_pos] = pathfind(G, F, sigma, v, A_pos, dests)
+% PATHFIND Carries a step of path finding out by taking into account the spread
+%          of fire and G (likability of area).
+
 	% Set initial parameters
 	dt = 1; % Time step
 
-	h = size(G, 1);
-	w = size(G, 2);
+	h      = size(G, 1);
+	w      = size(G, 2);
+	nagent = size(A_pos, 1);
 
-	A_dest = zeros(size(A_pos));
-	% newDest(); % Set destination of agent
+	% Set first destination in list to be dest of all agents
+	A_dest = repmat(dests{1}, nagent, 1);
+
+	% Set nearest destination to be dest of agents
+	newDest(A_pos, A_dest, dests);
+
+	% Matrix of sigma values, in matrix form. Can use a grid of variable
+	% visibility in the future.
+	sigma = sigma * ones(h,w);
 
 	% Move agent
 	V      = calcV(G, A_pos, sigma);
